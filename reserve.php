af2e721864d4fbce18d027efdce375167fe544c9
Temur Komilov <itteamdev74@gmail.com>
2023-03-06 20:10:58 +0500

    save posted data to sql

---
+++ reserve.php
@@ -0,0 +1,19 @@
+<?php
+if (!empty($_POST)) {
+	$date = $_POST['date'];
+	$time = $_POST['time'];
+	$guests = $_POST['guests'];
+	$additional = $_POST['additional'];
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$phone = $_POST['phone'];
+	try {
+		$pdo = new PDO("mysql:host=localhost;dbname=restaurant", 'root', 'root');
+		$sql = "INSERT INTO reserve (date, time, guests, additional, name, email, phone) VALUES ('$date', '$time', '$guests', '$additional', '$name', '$email', '$phone')";
+		$query = $pdo->prepare($sql);
+		$query->execute();
+	} catch (PDOException $e) {
+		die($e->getMessage());
+	}
+}
+?>
